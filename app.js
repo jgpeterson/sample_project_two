f801191f2285a8c6a7e376b184ac480c1b738c47
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-27 14:03:19 -0400

    added a companies index route, and schemas and seeds for both

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ db.once('open', () => {
 })
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -37,8 +37,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+const companyController = require('./routes/companyController')
+app.use('/companies', companyController)
 
+const snowboardController = require('./routes/snowboardController')
+app.use('/companies/:companyId/snowboards', snowboardController)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
